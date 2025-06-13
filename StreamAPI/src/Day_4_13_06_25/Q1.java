89f80b4cf1eec9df187fd77fa374c5269e501315
smhil <smhil@smhilal>
2025-06-13 12:45:18 +0530

    Adding 1Q

--- StreamAPI/src/Day_4_13_06_25/Q1.java
+++ StreamAPI/src/Day_4_13_06_25/Q1.java
@@ -1,22 +1,38 @@
 package Day_4_13_06_25;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+record Person(String name,Integer age) {
+	
+}
+
 
 public class Q1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		List<Person> list = new ArrayList<>();
 
 		System.out.println("Enter number of persons:");
-		int numOfp = Integer.parseInt(sc.next());
+		int numOfp = Integer.parseInt(sc.nextLine());
 
-		for (int i = 0; i < numOfp; i++) {
+		for (int i = 1; i <=numOfp; i++) {
 			System.out.println("Enter name of person " + i + ": ");
-			String name = sc.nextLine();
+			 //sc.nextLine();
+			 String name  = sc.nextLine();
 			System.out.println("Enter age of person " + i + ": ");
-			int age = sc.nextInt();
+			int age =Integer.parseInt(sc.nextLine());
+			list.add(new Person(name, age));
+			
+			
 
 		}
+		System.out.println("Sorted by age (ascending):");
+		
+		list.stream().sorted((p1,p2)->p1.age()-p2.age()).forEach(p-> System.out.println(p.name()+"("+p.age()+")"));
 		
 		
 		
---
+++ StreamAPI/src/Day_4_13_06_25/Q2.java
@@ -0,0 +1,36 @@
+package Day_4_13_06_25;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+record Employee(Integer id, String name, Double salary) {
+
+}
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		List<Employee> list = new ArrayList<>();
+
+		System.out.print("Enter number of employees: ");
+		int NoOfEmp = Integer.parseInt(sc.nextLine());
+
+		for (int i = 1; i <= NoOfEmp; i++) {
+			System.out.println("Enter ID of employee " + i + ": ");
+			int id = Integer.parseInt(sc.nextLine());
+			System.out.println("Enter name of employee " + i + ": ");
+			String name = sc.nextLine();
+			System.out.println("Enter salary of employee " + i + " :");
+			double salary = Double.parseDouble(sc.nextLine());
+				
+			list.add(new Employee(id, name, salary));
+			
+			
+		}
+
+	}
+
+}
